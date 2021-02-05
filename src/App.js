a6f62fa53a69e7c99377e7e45aac7f23af154cd0
TomasMoratoPerezPorro <tomasmpp@gmail.com>
2021-02-05 12:42:50 +0100

    Fetch Data from MOCKUP server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header.js';
 import Tasks from './components/Tasks.js';
 import AddTask from './components/AddTask.js';
@@ -7,20 +7,57 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  };
+
+  // Fetch single Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   // Show Form
   const toggleShowAddTask = () => {
     setShowAddTask(!showAddTask);
   };
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      // Parse javascript object to JSON
+      body: JSON.stringify(task)
+    });
+
+    //Get the returned data, aka the saved task
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
   };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
